483633afaf5807ace51e994137f374ac5e34e199
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-11-30 15:57:22 +1100

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,11 @@
+import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.UI.SDL.Image as SDLI
+
+main :: IO ()
+main = do
+    screen <- SDL.setVideoMode 640 480 32 [SDL.SWSurface]
+    hello <- SDLI.load "image.png"
+
+    SDL.blitSurface hello Nothing screen Nothing
+    SDL.flip screen
+    SDL.delay 2000
